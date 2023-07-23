40e0678b4553766efff5b148ce42ba18cd3ac239
Cody Thompson <cjthomp007@gmail.com>
2023-07-23 13:04:49 -0600

    fix all bugs with game not following the loop and invalid references

--- game.js
+++ game.js
@@ -1,49 +1,53 @@
-function computerPlay() {
-    choose = Math.floor(Math.random()*3)+1;
-    if (choose == 1) {
-        comSelection = 'scissors'
-    } else if ( choose == 2) {
-        comSelection = 'rock'
-    } else if ( choose == 3) {
-        comSelection = 'paper'
+let roundsPlayed = 0; // Add a variable to keep track of the rounds played
+
+function comSel() {
+    choose = Math.floor(Math.random() * 3) + 1;
+    if (choose === 1) {
+        return 'scissors';
+    } else if (choose === 2) {
+        return 'rock';
+    } else if (choose === 3) {
+        return 'paper';
     }
 }
-function plySel(event) {
+
+function plyrSel(event) {
     const button = event.target;
     playerSelection = button.textContent.toLowerCase();
+    game();
 }
-function playGame() {
-    console.log(`Computer picked ${comSelection}`)
-    console.log(`you picked ${playerSelection}`)
-  if (comSelection === playerSelection){
-        console.log(`It's a draw!`)
-
-} else if (
-    (comSelection == 'rock' && playerSelection == 'paper') ||
-    (comSelection == 'scissors' && playerSelection == 'rock') ||
-    (comSelection == 'paper' && playerSelection == 'scissors')
-){
-    console.log(`You picked ${playerSelection} and it beats ${comSelection}, you win!`)
-} else {
-    console.log(`Computer picked ${comSelection} and it beats ${playerSelection}, you lose!`)
-}}
 
 const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 
-rockButton.addEventListener('click', plySel);
-paperButton.addEventListener('click', plySel);
-scissorsButton.addEventListener('click', plySel);
+rockButton.addEventListener('click', plyrSel);
+paperButton.addEventListener('click', plyrSel);
+scissorsButton.addEventListener('click', plyrSel);
 
-document.addEventListener('click', function (e) {
-if (e.target.matches('button')) {
-    computerPlay();
-    playGame();
+function playGame() {
+    const comSelection = comSel();
+    console.log(`Computer picked ${comSelection}`);
+    console.log(`You picked ${playerSelection}`);
+
+    if (comSelection === playerSelection) {
+        console.log(`It's a draw!`);
+    } else if (
+        (comSelection === 'rock' && playerSelection === 'paper') ||
+        (comSelection === 'scissors' && playerSelection === 'rock') ||
+        (comSelection === 'paper' && playerSelection === 'scissors')
+    ) {
+        console.log(`You picked ${playerSelection} and it beats ${comSelection}, you win!`);
+    } else {
+        console.log(`Computer picked ${comSelection} and it beats ${playerSelection}, you lose!`);
+    }
 }
-})
-// for (let i=0;i<5; i++) {
-// plySel();
-// computerPlay();
-// playGame();
-// }
+
+function game() {
+    if (roundsPlayed < 5) {
+        roundsPlayed++;
+        console.log(`Round ${roundsPlayed}`);
+        comSel();
+        playGame();
+    }
+}
